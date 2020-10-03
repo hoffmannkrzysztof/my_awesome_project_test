90b701eb21491d37c131b096b594eb8838fdc4f5
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2020-10-03 15:17:31 +0200

    Update conftest.py

--- my_awesome_project/conftest.py
+++ my_awesome_project/conftest.py
@@ -7,8 +7,6 @@ from my_awesome_project.users.tests.factories import UserFactory
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
     settings.MEDIA_ROOT = tmpdir.strpath
-
-
 @pytest.fixture
 def user() -> User:
     return UserFactory()
