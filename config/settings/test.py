bb4487e20cc862603718c4040a14ca95bd77bfe8
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2020-10-03 15:16:47 +0200

    add linter.yml

--- config/settings/test.py
+++ config/settings/test.py
@@ -49,14 +49,3 @@ EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 
 # Your stuff...
 # ------------------------------------------------------------------------------
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
